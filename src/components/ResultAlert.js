d25c5845692d2f5b7917c6803c9e3f46b9acb2f8
trav164 <travis164@gmail.com>
2021-04-22 20:48:25 +0100

    Update ResultAlert.js

--- src/components/ResultAlert.js
+++ src/components/ResultAlert.js
@@ -3,7 +3,6 @@ import React from "react";
 const ResultAlert = (props) => {
   // Wouldn't send the full parent state to child component
   // was not being returned if result was 0
-  console.log(props.result);
   if (props.result) {
     return <h1>Result: {props.result}</h1>;
   }
